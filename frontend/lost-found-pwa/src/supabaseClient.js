7cfb23b714c265cac002a690d465b8c68143a974
saurabh singh <saurabhsinghnegi71@gmail.com>
2026-03-09 20:00:44 +0530

    Secure API keys

--- frontend/lost-found-pwa/src/supabaseClient.js
+++ frontend/lost-found-pwa/src/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ixexerxfjbbkidcqafpw.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4ZXhlcnhmamJia2lkY3FhZnB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5ODI4MTAsImV4cCI6MjA4NjU1ODgxMH0.xHak7Pxc_uW92FrSYJhVhsz_1ZmvOR-JHRgfpZNHwxo";
+const supabaseUrl =
+    import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey =
+    import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
