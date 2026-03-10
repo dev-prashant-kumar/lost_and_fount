e8e562d19adea023c6f6b0ebc053198ed10620b3
saurabh singh <saurabhsinghnegi71@gmail.com>
2026-03-10 19:12:46 +0530

    change vite.config.js file icon for mobile

--- frontend/lost-found-pwa/vite.config.js
+++ frontend/lost-found-pwa/vite.config.js
@@ -1,9 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+
+            manifest: {
+                name: 'Lost & Found App',
+                short_name: 'LostFound',
+                description: 'Lost and Found Application',
+                theme_color: '#0f172a',
+                background_color: '#0f172a',
+                display: 'standalone',
+                start_url: '/',
+                icons: [{
+                        src: '/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/logoimage.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
 })
